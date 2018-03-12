247d1e8d194972fa395e3613b5b03a544c0a4553
Pascal Martineau <pascal@lewebsimple.ca>
2018-03-12 19:11:03 -0400

    Resolve ui/ and application/ in tns-code-modules

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = (context, request, callback) => {
+
   if (context.indexOf('tns-core-modules') !== -1
-      || context.indexOf('nativescript-') !== -1
-      || /^(tns-core-modules)/i.test(request)) {
-    // support plugins requiring sibling files by rewriting fi. "./barcodescanner-common" to "nativescript-barcodescanner/barcodescanner-common"
-    if (request.indexOf("./") === 0) {
+    || context.indexOf('nativescript-') !== -1
+    || /^(tns-core-modules)/i.test(request)
+    || /^(ui|application)/i.test(request)) {
+
+    // Support plugins requiring sibling files by rewriting './' into 'context/'
+    if (request.indexOf('./') === 0) {
       request = path.join(path.basename(context), request.substring(2));
     }
+
     return callback(null, 'commonjs ' + request);
   }
+
   callback();
 };
