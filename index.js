9517818820b60b94bf0fc3437ea70ca81aaca47a
mehyaa <mehyaa@gmail.com>
2018-04-23 13:20:28 +0300

    Fix internal module requring

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ module.exports = (context, request, callback) => {
 
   if (context.indexOf('tns-core-modules') !== -1
     || context.indexOf('nativescript-') !== -1
-    || /^(tns-core-modules)/i.test(request)
+    || /^tns-core-modules/i.test(request)
     || /^(ui|application)/i.test(request)) {
 
-    // Support plugins requiring sibling files by rewriting './' into 'context/'
-    if (request.indexOf('./') === 0) {
-      request = path.join(path.basename(context), request.substring(2));
+    if (request.indexOf('../') === 0 || request.indexOf('./') === 0) {
+      let index = context.indexOf('node_modules') + 'node_modules'.length + 1;
+
+      request = path.normalize(path.join(context.substring(index).replace(/\\/g, '/'), request)).replace(/\\/g, '/');
+    }
+    else if (/^(ui|application)/i.test(request)) {
+      request = `tns-core-modules/${request}`.replace(/\\/g, '/');
     }
 
     return callback(null, 'commonjs ' + request);
